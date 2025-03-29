b27a464ace35708f6f698a076649da2c3328efc0
SUBHAMOY SASMAL <145684110+subhamoy222@users.noreply.github.com>
2025-03-29 16:57:29 +0530

    backend2

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
-// import express from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-const express = require('express');
-
 
 // Routes
 import userRoutes from './routes/userRoutes.js';
